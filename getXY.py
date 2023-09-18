8139569daf4697ea392925aa48e71c83407b4fc1
HyunahJung <118616320+jha0316@users.noreply.github.com>
2023-09-19 02:33:50 +0900

    Update getXY.py
    second commit

--- getXY.py
+++ getXY.py
@@ -1,11 +1,31 @@
+import pandas as pd
 import requests
 
-location = "서울특별시 도봉구 삼양로144길 33" # 덕대 위치 ########여기다가 각 음식점의 주소 넣기#######
-url = f"https://dapi.kakao.com/v2/local/search/address.json?query={location}"
-kakao_key = "f3-----------------" 
-result = requests.get(url, headers={"Authorization":f"KakaoAK {kakao_key}"})
-json_obj = result.json()
-#print(json_obj)
-x = json_obj['documents'][0]['x']
-y = json_obj['documents'][0]['y']
-print(x , y)
+#매장정보가 담긴 csv명이 stores.csv일 경우
+df = pd.read_csv('stores.csv', encoding='cp949')
+
+kakao_key = "f3b121a900d2f12872ba8c70acff4995"
+
+def get_coordinates(address):
+    url = f"https://dapi.kakao.com/v2/local/search/address.json?query={address}"
+    result = requests.get(url, headers={"Authorization": f"KakaoAK {kakao_key}"})
+    json_obj = result.json()
+    if json_obj.get('documents'):
+        x = json_obj['documents'][0]['x']
+        y = json_obj['documents'][0]['y']
+        return x, y
+    else:
+        return None, None
+
+x_coordinates = []
+y_coordinates = []
+
+for address in df['storesaddress']:
+    x, y = get_coordinates(address)
+    x_coordinates.append(x)
+    y_coordinates.append(y)
+
+df['x'] = x_coordinates
+df['y'] = y_coordinates
+
+df.to_csv('stores_with_xy.csv', index=False, encoding='utf-8-sig')
